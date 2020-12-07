1198a36a9029bd4c937015382ef11834e5d123b3
GreenGrace <s.b.mokrov@gmail.com>
2020-12-07 16:17:06 +0300

    add Kafka logging

--- src/main/java/is/toxic/service/UserDetailsServiceImpl.java
+++ src/main/java/is/toxic/service/UserDetailsServiceImpl.java
@@ -5,7 +5,6 @@ import is.toxic.dao.AppUserDAO;
 import is.toxic.entity.AppUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.GrantedAuthority;
